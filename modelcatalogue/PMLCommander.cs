94f69d444eb890211a7f4f02ff0dc08d3934d9b7
land297 <46065999+land297@users.noreply.github.com>
2020-11-30 07:08:42 +0100

    Add files via upload

---
+++ modelcatalogue/PMLCommander.cs
@@ -0,0 +1,39 @@
+﻿using Aveva.Core.Database;
+using Aveva.Core.Utilities.CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace modelcatalogue {
+    public class PMLCommander {
+        public static bool RunPMLCommandInParentheses(DbElement ce, string attribute, string command, out string error) {
+            return RunPMLCommand(ce, attribute, $"({command})", out error);
+        }
+        public static  bool RunPMLCommand(DbElement ce, string attribute, string command, out string error) {
+            if (string.IsNullOrWhiteSpace(command)) {
+                error = "Command is empty";
+                return false;
+            }
+            CurrentElement.Element = ce;
+            Command pmlCommand;
+
+            pmlCommand = Command.CreateCommand(attribute + " " + command);
+
+            var result = pmlCommand.RunInPdms();
+            error = pmlCommand.Error.MessageText();
+            return result;
+        }
+        public static bool RunPMLCommand(string command) {
+            if (string.IsNullOrWhiteSpace(command)) {
+                return false;
+            }
+            Command pmlCommand;
+
+            pmlCommand = Command.CreateCommand(command);
+
+            var result = pmlCommand.RunInPdms();
+            return result;
+        }
+    }
+}
